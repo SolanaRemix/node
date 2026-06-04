8e194f867d22724bfddc8af81cca30257c5a93b6
Joseph <cast.quest@icloud.com>
2026-06-05 01:09:01 +0800

    Update commitlint.config.cjs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,32 +1,32 @@
-﻿// commitlint.config.js - Enforces Conventional Commits
+// commitlint.config.js - Enforces Conventional Commits
 module.exports = {
-extends: ['@commitlint/config-conventional'],
-rules: {
-'type-enum': [2, 'always', [
-'feat', // New feature
-'fix', // Bug fix
-'docs', // Documentation
-'style', // Code style
-'refactor', // Code refactor
-'perf', // Performance
-'test', // Testing
-'chore', // Maintenance
-'ci', // CI/CD
-'build', // Build system
-'revert' // Revert commit
-]],
-'scope-enum': [2, 'always', [
-'blockchain',
-'surgery',
-'tests',
-'security',
-'api',
-'dashboard',
-'release',
-'deps'
-]],
-'subject-case': [2, 'always', 'sentence-case'],
-'header-max-length': [2, 'always', 100]
-},
-helpUrl: 'https://www.conventionalcommits.org/'
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat',     // New feature
+      'fix',      // Bug fix
+      'docs',     // Documentation
+      'style',    // Code style
+      'refactor', // Code refactor
+      'perf',     // Performance
+      'test',     // Testing
+      'chore',    // Maintenance
+      'ci',       // CI/CD
+      'build',    // Build system
+      'revert'    // Revert commit
+    ]],
+    'scope-enum': [2, 'always', [
+      'blockchain',
+      'surgery',
+      'tests',
+      'security',
+      'api',
+      'dashboard',
+      'release',
+      'deps'
+    ]],
+    'subject-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 100]
+  },
+  helpUrl: 'https://www.conventionalcommits.org/'
 };
